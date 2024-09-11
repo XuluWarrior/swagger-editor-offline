553bedd3951608559b7057ee25f31683f5f2e4b3
XuluWarrior <github@metaliving.plus.com>
2024-09-12 00:13:03 +0100

    Package and use the swagger-editor-bundle.js Copied from node_modules/swagger-editor-dist
    Importing the bundles resulted in vite doing funny things during the build and the built app no longer being able to run them.

--- src/renderer/src/index.ts
+++ src/renderer/src/index.ts
@@ -1,5 +1,3 @@
-import SwaggerEditorBundle from 'swagger-editor-dist/swagger-editor.js'
-import SwaggerEditorStandalonePreset from 'swagger-editor-dist/swagger-editor-standalone-preset.js'
 import 'swagger-editor-dist/swagger-editor.css'
 
 window.onload = function() {
