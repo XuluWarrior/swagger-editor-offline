ef322787b48d1a030d182062c24c5166e89d4d22
XuluWarrior <github@metaliving.plus.com>
2024-09-12 00:20:39 +0100

    Add SwaggerEditorBundle types to Window type

--- src/renderer/src/index.ts
+++ src/renderer/src/index.ts
@@ -1,14 +1,24 @@
 import 'swagger-editor-dist/swagger-editor.css'
+import SwaggerEditorBundle from "swagger-editor-dist/swagger-editor-bundle.js";
+import SwaggerEditorStandalonePreset from "swagger-editor-dist/swagger-editor-standalone-preset.js";
 
 window.onload = function() {
   // Build a system\
-  const editor = SwaggerEditorBundle({
+  const editor = window.SwaggerEditorBundle({
     dom_id: '#swagger-editor',
     layout: 'StandaloneLayout',
     presets: [
-      SwaggerEditorStandalonePreset
+      window.SwaggerEditorStandalonePreset
     ]
   });
 
   window.editor = editor
+}
+
+declare global {
+  interface Window {
+    SwaggerEditorBundle: typeof SwaggerEditorBundle;
+    SwaggerEditorStandalonePreset: typeof SwaggerEditorStandalonePreset
+    editor: ReturnType<SwaggerEditorBundle>
+  }
 }
