9448fc99e195c74872a72339fdb1f1a54cea8a68
XuluWarrior <github@metaliving.plus.com>
2024-09-23 01:02:22 +0200

    Help menus

--- src/main/menus.ts
+++ src/main/menus.ts
@@ -1,4 +1,4 @@
-import { Menu } from 'electron'
+import { Menu, shell } from 'electron'
 
 const template = [
   { role: 'appMenu'},
@@ -6,7 +6,45 @@ const template = [
   { role: 'editMenu' },
   { role: 'viewMenu' },
   { role: 'windowMenu' },
-  { role: 'help' }
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Swagger Editor Offline',
+        submenu: [
+          {
+            label: 'View on GitHub',
+            click: async () => {
+              await shell.openExternal('https://github.com/XuluWarrior/swagger-editor-offline')
+            }
+          }
+        ]
+      },
+      {
+        label: 'Swagger Editor',
+        submenu: [
+          {
+            label: 'About Swagger Editor',
+            click: async () => {
+              await shell.openExternal('https://swagger.io/tools/swagger-editor/')
+            }
+          },
+          {
+            label: 'View Docs',
+            click: async () => {
+              await shell.openExternal('https://swagger.io/docs/open-source-tools/swagger-editor/')
+            }
+          },
+          {
+            label: 'View on GitHub',
+            click: async () => {
+              await shell.openExternal('https://github.com/swagger-api/swagger-editor')
+            }
+          }
+        ]
+      },
+    ]
+  }
 ]
 
 const menu = Menu.buildFromTemplate(template)
