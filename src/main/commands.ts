b4734b3669315db93701fb6c2c8d1f48f5bbcd23
XuluWarrior <github@metaliving.plus.com>
2024-09-23 21:56:15 +0200

    Save As menu command Supports saving/converting as either JSON or YAML

--- src/main/commands.ts
+++ src/main/commands.ts
@@ -1,6 +1,7 @@
-import {BrowserWindow, dialog} from 'electron'
+import {BrowserWindow, dialog, ipcMain} from 'electron'
 import prompt from "electron-prompt";
 import { promises as fs } from 'fs'
+import path from 'path'
 import isJsonObject from "is-json"
 import { dump, load } from "js-yaml"
 import request from 'phin'
@@ -48,6 +49,45 @@ export async function importFile() {
     } catch(e: any) {
       dialog.showErrorBox('Error loading file', `Oof! There was an error loading your document:\n\n${e.message || e}`)
     }
+  }
+}
+
+function getSpecLanguage(spec){
+  return spec.trim()[0] === "{" ? "json" : "yaml"
+}
+
+export async function saveAs() {
+  const window = BrowserWindow.getFocusedWindow()!
+  window.webContents.send("send-spec")
+
+  const spec  = await new Promise<string>((resolve, reject) => {
+    const handleSpecStr = (_event, sentSpec) => {
+      resolve(sentSpec);
+    }
+    ipcMain.once('spec-str', handleSpecStr)
+    setTimeout(() => {
+      ipcMain.off('spec-str', handleSpecStr)
+      reject("Window didn't respond with spec");
+    }, 1000)
+  })
+
+  const specLang = getSpecLanguage(spec)
+  const filters = [
+    { name: 'YAML', extensions: ['yml'] },
+    { name: 'JSON', extensions: ['json'] }
+  ]
+  const selected = await dialog.showSaveDialog({
+    filters: specLang === 'yaml' ? filters : filters.reverse(),
+    properties: ['createDirectory']
+  })
+
+  if (!selected.canceled) {
+    const yamlContent = specLang === 'yaml' ? spec : dump(JSON.parse(spec))
+    const jsonContent = specLang === 'json' ? spec : JSON.stringify(load(spec), null, 2)
+
+    const fileContent = (path.extname(selected.filePath) === '.json') ? jsonContent : yamlContent
+
+    fs.writeFile(selected.filePath, fileContent, 'utf-8')
   }
 }
 
